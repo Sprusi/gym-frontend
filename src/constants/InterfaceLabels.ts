a69a3dea2b23c1851833c48057c135a50cb2c387
brovko.dr <brovko@atb.su>
2025-03-18 23:59:15 +1000

    init

---
+++ src/constants/InterfaceLabels.ts
@@ -0,0 +1,10 @@
+/* eslint-disable i18n/no-russian-character */
+
+/**
+ * Animation Page
+ */
+export const AP_TITLE_FIRST = 'ТВОЙ РИНГ';
+export const AP_TITLE_SECOND = 'ТВОЯ ПОБЕДА';
+export const AP_MOTIVATIONAL_QUOTE =
+  'Мотивация заключается в Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio sunt, eos dolore voluptatibus distinctio autem cupiditate corrupti temporibus doloremque id iste soluta magni, laudantium commodi quam quo dicta accusantium consequatur!';
+export const AP_START_BUTTON = 'Начать';
